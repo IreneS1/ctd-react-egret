e531054cf57d678e4f6fb4ef7a5a6a023337c347
Irene Sanchez <irene.iscc777@gmail.com>
2021-08-20 15:05:10 -0700

    Added new components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import React from 'react';
-
-const todoList = [
-  {
-    id: 1,
-    title: 'Write Lesson 1.1 instructions',
-  },
-  {
-    id: 2,
-    title: 'Submit pull request',
-  },
-  {
-    id: 3,
-    title: 'Respond to introduction email',
-  },
-];
+import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
 
 function App() {
   return (
     <div>
       <h1>Todo List</h1>
-
-      <ul>
-        {todoList.map(function (item) {
-          return <li key={item.id}>{item.title}</li>;
-        })}
-      </ul>
-
+      <AddTodoForm />
+      <TodoList />
     </div>
   );
 }
