0a39d80a360c157337c95348d81e2e1f1b4be089
Irene Sanchez <irene.iscc777@gmail.com>
2021-09-23 23:23:08 -0400

    lesson 5 done

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import React from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
+const useSemiPersistentState = () => {
+  const [todoList, setTodoList] = React.useState(
+    JSON.parse(localStorage.getItem('savedTodoList')) || []
+  );
+    // define a useEffect React hook
+    React.useEffect(() => {
+      localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+    });
+
+    return [todoList, setTodoList];
+};
+
 function App() {
 
-  const [todoList, setTodoList] = React.useState([]);
+  const [todoList, setTodoList] = useSemiPersistentState();
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
